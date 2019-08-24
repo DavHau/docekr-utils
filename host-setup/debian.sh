6712f279f2316c8f045ebb317f8759581009c7f9
DavHau <42246742+DavHau@users.noreply.github.com>
2019-08-24 14:31:15 +0200

    Update debian.sh

--- host-setup/debian.sh
+++ host-setup/debian.sh
@@ -15,3 +15,8 @@ add-apt-repository \
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 docker run hello-world
+
+compose_bin=/usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o $compose_bin
+chmod +x $compose_bin
+ln -s /usr/local/bin/dco $compose_bin
